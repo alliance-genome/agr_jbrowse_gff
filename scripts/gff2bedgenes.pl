071011cc8cbcef2f2e79b77cd9282b060dde4d74
Scott Cain <scott+git@scottcain.net>
2023-10-30 16:56:35 -0700

    silencing undef warnings

--- scripts/gff2bedgenes.pl
+++ scripts/gff2bedgenes.pl
@@ -14,6 +14,7 @@ while (<IN>) {
 	my @line = split /\t/;
 	next if $line[2] ne 'gene';
 	my $name = $1 if ($line[8] =~ /Name=([^;]+);/ || $line[8] =~ /Name=([^;]+)$/);
+        next unless $name;
 	print OUT join("\t", $line[0], $line[3]-1, $line[4], $name, 0, $line[6]), "\n";
 }
 
