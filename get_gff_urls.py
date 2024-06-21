910ef7a73146fb5f37a3c31c8a0888428e3251ea
Scott Cain <scott+git@scottcain.net>
2024-06-21 17:03:54 +0000

    trying python subprocess to curl the GFF files

--- get_gff_urls.py
+++ get_gff_urls.py
@@ -2,9 +2,11 @@
 
 import sys
 import json
+import subprocess
 
 data = json.load(sys.stdin)
 
 for d in data:
-    print("curl -O "+d['s3Url']+"\n")
+    returned_value = subprocess.call("curl -O "+d['s3Url'], shell=True)
+    print (returned_value)
 
